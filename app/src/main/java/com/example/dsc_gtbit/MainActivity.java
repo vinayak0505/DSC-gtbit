646733473bc2e8d5f33ece914760b162b64d58ef
Vinayak agarwal <vinayakaggarwal05@gmail.com>
2020-10-24 21:48:03 +0530

    first commit

---
+++ app/src/main/java/com/example/dsc_gtbit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dsc_gtbit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
